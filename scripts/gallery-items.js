7d26ce1eabcf086e19a6f7176a3253386d91712c
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-08-09 13:34:20 +0300

    gallery

---
+++ scripts/gallery-items.js
@@ -0,0 +1,44 @@
+export const galleryItems = [
+  {
+    preview:
+    '../img/gal1.webp',
+    original:
+      '../img/gal1.webp',
+    description: 'Хочу гулять',
+  },
+  {
+    preview:
+      '../img/gal2.webp',
+    original:
+      '../img/gal2.webp',
+    description: 'Хочу лежати',
+  },
+  {
+    preview:
+      '../img/gal3.webp',
+    original:
+      '../img/gal3.webp',
+    description: 'Хочу лежати',
+  },
+  {
+    preview:
+      '../img/gal4.webp',
+    original:
+      '../img/gal4.webp',
+    description: 'Взагалі нічого не хочу',
+  },
+  {
+    preview:
+      '../img/gal5.webp',
+    original:
+      '../img/gal5.webp',
+    description: 'Третім будеш?',
+  },
+  {
+    preview:
+      '../img/gal6.webp',
+    original:
+      '../img/gal6.webp',
+    description: 'Третім будеш?',
+  },
+];
